ba187dc25758fe3449fce604c27d31c1674b73a3
juliet422 <82244048+juliet422@users.noreply.github.com>
2021-10-30 23:00:19 -0500

    Add files via upload

---
+++ sim_moneda/src/sim_moneda/Sim_moneda.java
@@ -0,0 +1,13 @@
+package sim_moneda;
+
+import java.util.Random;
+import javax.swing.ImageIcon;
+public class Sim_moneda {
+
+    
+    public static void main(String[] args) {
+        Random rand = new Random();
+        
+    }
+    
+}
